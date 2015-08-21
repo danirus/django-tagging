7c17f04a8b419be15560f95b5e103875a2461800
Daniel Rus Morales <danirus@eml.cc>
2015-08-21 08:49:44 +0200

    Add compat with Django 1.4 LTS

--- tagging/models.py
+++ tagging/models.py
@@ -9,7 +9,13 @@ try:
 except NameError:
     from sets import Set as set
 
-from django.contrib.contenttypes import generic
+from django import VERSION as DJANGO_VERSION
+
+if DJANGO_VERSION[:2] > (1, 6):
+    from django.contrib.contenttypes.fields import GenericForeignKey
+else:
+    from django.contrib.contenttypes.generic import GenericForeignKey
+    
 from django.contrib.contenttypes.models import ContentType
 from django.db import connection, models
 from django.db.models.query import QuerySet
@@ -492,7 +498,7 @@ class TaggedItem(models.Model):
     tag          = models.ForeignKey(Tag, verbose_name=_('tag'), related_name='items')
     content_type = models.ForeignKey(ContentType, verbose_name=_('content type'))
     object_id    = models.PositiveIntegerField(_('object id'), db_index=True)
-    object       = generic.GenericForeignKey('content_type', 'object_id')
+    object       = GenericForeignKey('content_type', 'object_id')
 
     objects = TaggedItemManager()
 
